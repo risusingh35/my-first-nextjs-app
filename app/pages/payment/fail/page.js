4c42c312127924cdca62a3daf9d90f4b5de936c3
risusingh35 <risusingh35@gmail.com>
2024-05-27 14:52:54 +0530

    success and fail payment page added

---
+++ app/pages/payment/fail/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FailurePage = () => {
+    return (
+        <div>
+            <h1>Payment Failed</h1>
+            <p>Oops! Something went wrong with your payment.</p>
+        </div>
+    );
+};
+
+export default FailurePage;
